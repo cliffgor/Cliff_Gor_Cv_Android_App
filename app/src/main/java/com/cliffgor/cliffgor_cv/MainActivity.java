69b07cb602cfe177e6e7027f2644d950f37f4b29
cliffgor <clierozigy@gmail.com>
2020-01-23 13:27:30 +0300

    set the fragment home to start as the first screen

--- app/src/main/java/com/cliffgor/cliffgor_cv/MainActivity.java
+++ app/src/main/java/com/cliffgor/cliffgor_cv/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cliffgor.cliffgor_cv.home.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar() .hide();
+        setHomeFragment();
+    }
+
+
+
+
+    void setHomeFragment (){
+        getSupportFragmentManager().beginTransaction().replace(R.id.conainer, new HomeFragment()).commit() ;
     }
 }
--- app/src/main/java/com/cliffgor/cliffgor_cv/home/HomeFragment.java
+++ app/src/main/java/com/cliffgor/cliffgor_cv/home/HomeFragment.java
@@ -1,6 +1,22 @@
 package com.cliffgor.cliffgor_cv.home;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.cliffgor.cliffgor_cv.R;
+
 public class HomeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container, false);
+    }
 }
